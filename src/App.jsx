89052447be6c9e592be9e18956d37ab040d811d3
Oshada Nethmina <oshadanethmina665@gmail.com>
2026-03-13 23:03:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AdminRoute from './components/AdminRoute'
+
+function App() {
+  
+
+  return (
+    <>
+      <AdminRoute></AdminRoute>
+    </>
+  )
+}
+
+export default App
