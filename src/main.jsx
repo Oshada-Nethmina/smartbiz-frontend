5fd219159c94abb85ff044d806f208b12a47857d
Oshada Nethmina <oshadanethmina665@gmail.com>
2026-04-26 11:06:46 +0530

    create api services

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { Toaster } from 'react-hot-toast'
+import App from './App'
+import theme from './theme/theme'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            fontFamily: '"Plus Jakarta Sans", sans-serif',
+            borderRadius: 10,
+            fontSize: 14,
+            fontWeight: 500,
+          },
+          success: { iconTheme: { primary: '#10B981', secondary: '#fff' } },
+          error: { iconTheme: { primary: '#EF4444', secondary: '#fff' } },
+        }}
+      />
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </ThemeProvider>
+  </React.StrictMode>
 )
